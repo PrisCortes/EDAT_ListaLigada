59f33b39cfb66ed1097b11b6316a07fa0046feda
PrisCortes <23030285@itesa.edu.mx>
2024-10-28 09:20:18 -0600

    Versión_03: Se agregó el menu de opciones

--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,19 +1,64 @@
 ﻿using ListaLigada.Logica;
+using System.ComponentModel.Design;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World! LISTA LIGADA SIMPLE");
+        Console.WriteLine("LISTA LIGADA SIMPLE");
 
         var singleList = new SingleList <string> ();
-        singleList.Add ("Priscila");
-        singleList.Add ("Uriel");
-        singleList.Add ("Alechis");
-        singleList.Add ("César");
-        singleList.Add ("Aneth");
+        int option = 0;
+        do
+        {
+            option = Menu();
+            switch (option) {
+                case 1: AgregarElemento(); break;
+                case 2: MostrarLista(); break;
+                case 0: break;
+                default: Console.WriteLine("Esta opción no existe"); break;
+            }
 
-        Console.WriteLine(singleList);
+        } while (option != 0);
+
+        void AgregarElemento()
+        {
+            Console.Write("Escribir el nombre del nodo: ");
+            var data = Console.ReadLine();
+            singleList.Add(data);
+        }
+
+        void MostrarLista() {
+            Console.WriteLine("\nLa lista es:");
+            Console.WriteLine(singleList);
+        }
+
         Console.ReadKey();
+    }
+
+    private static int Menu()
+    {
+        Console.WriteLine("\nMENU");
+        Console.WriteLine("1. Agregar elemento");
+        Console.WriteLine("2. Mostrar lista");
+        Console.WriteLine("0. Salir");
+
+        bool esValido = false;
+        int option = 0;
+        do
+        {
+            Console.Write("Elige una opción del menu: ");
+            var opcionValida = Console.ReadLine();
+            if (!int.TryParse(opcionValida, out option) || option < 0 || option > 2)
+            {
+                Console.WriteLine("Digite una opción valida");
+                esValido = true;
+            }
+            else { 
+                esValido = true;
+            }
+        } 
+        while (esValido == false);  
+        return option;
     }
 }
