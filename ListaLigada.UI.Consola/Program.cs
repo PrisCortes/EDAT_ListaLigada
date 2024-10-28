3d946b8d2b43776833c4b449657ed06b866ebdd5
PrisCortes <23030285@itesa.edu.mx>
2024-10-28 08:38:45 -0600

    Versión_02: Método Agregar elemento y ToString.

--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,8 +1,19 @@
-﻿internal class Program
+﻿using ListaLigada.Logica;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World! LISTA LIGADA SIMPLE");
 
+        var singleList = new SingleList <string> ();
+        singleList.Add ("Priscila");
+        singleList.Add ("Uriel");
+        singleList.Add ("Alechis");
+        singleList.Add ("César");
+        singleList.Add ("Aneth");
+
+        Console.WriteLine(singleList);
+        Console.ReadKey();
     }
 }
